a601ad81422b5cafe6f6ca3e94cb7f45d77b121a
tajraab <tajrabacevac@gmail.com>
2023-10-30 19:11:48 +0100

    .

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import NavBar from"./components/NavBar/NavBar";
-
+import Baner from "./components/Baner/Baner";
 
 
 function App() {
---
+++ src/components/Baner/Baner.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+ function Baner() {
+  return (
+    <div>Baner</div>
+  )
+}
+export default Baner;
